86e151da19c6a0d368e2f0e3abcd0d99378a7947
Rajat Rawat <rajatrawat88@gmail.com>
2018-09-27 09:11:04 -0500

    python work

--- python/pythonProgram/DBapps/dbApp.py
+++ python/pythonProgram/DBapps/dbApp.py
@@ -21,6 +21,13 @@ def delete(item):
     conn.commit()
     conn.close()
 
+def update(quantity,price,item):
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE store SET quantity=(?), price=(?) WHERE item=(?)",(quantity,price,item))
+    conn.commit()
+    conn.close()
+
 def view():
     conn=sqlite3.connect("lite.db")
     cur=conn.cursor()
@@ -29,7 +36,7 @@ def view():
     conn.close()
     return rows
 
-choice=input("Press 1 to enter data \n Press 2 to delete data \n Press 3 to view data \n")
+choice=input("Press 1 to enter data \n Press 2 to delete data \n Press 3 to update data \n Press 4 to view data \n")
 if choice == "1":
     item=input("Enter the name of ITEM ")
     quantity=int(input("Enter Quantity "))
@@ -41,4 +48,10 @@ elif choice == "2":
     delete(item) # or insert("Wine Glass")
 
 elif choice == "3":
+    item=input("Enter the name of ITEM which you want to modify ")
+    quantity=int(input("Enter Quantity "))
+    price=float(input("Enter Price "))
+    update(quantity,price,item) # or insert("Wine Glass")
+
+elif choice == "4":
     print(view())
