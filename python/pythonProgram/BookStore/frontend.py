938caf2e6fc8c76679e254b567a397539be024fc
Rajat Rawat <rajatrawat88@gmail.com>
2018-12-07 00:10:19 -0600

    Completed the project

--- python/pythonProgram/BookStore/frontend.py
+++ python/pythonProgram/BookStore/frontend.py
@@ -14,8 +14,40 @@ def view_command():
     for row in backend.view():
         list1.insert(END,row)
 
+def search_command():
+    list1.delete(0,END)
+    for row in backend.search(Title_text.get(),Author_text.get(),Year_text.get(),ISBN_text.get()):
+        list1.insert(END,row)
+
+def add_command():
+    list1.delete(0,END)
+    backend.insert(Title_text.get(),Author_text.get(),Year_text.get(),ISBN_text.get())
+    list1.insert(END,(Title_text.get(),Author_text.get(),Year_text.get(),ISBN_text.get()))
+
+def get_selected_row(event):
+    global selected_tuple
+    index=list1.curselection()[0]
+    selected_tuple=list1.get(index)
+    e1.delete(0,END)
+    e1.insert(0,selected_tuple[1])
+    e2.delete(0,END)
+    e2.insert(0,selected_tuple[2])
+    e3.delete(0,END)
+    e3.insert(0,selected_tuple[3])
+    e4.delete(0,END)
+    e4.insert(0,selected_tuple[4])
+    #print(selected_tuple)
+
+def delete_command():
+    backend.delete(selected_tuple[0])
+
+def update_command():
+    backend.update(selected_tuple[0],Title_text.get(),Author_text.get(),Year_text.get(),ISBN_text.get())
+    print(selected_tuple[0],selected_tuple[1],selected_tuple[2],selected_tuple[3],selected_tuple[4])
 
 window = Tk()
+
+window.wm_title("BookStore")
 
 l1=Label(window,text="Title")
 l1.grid(row=0,column=0)
@@ -54,23 +86,25 @@ sb1.grid(row=2,column=2,rowspan=6)
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
+list1.bind('<<ListboxSelect>>',get_selected_row)
+
 b1=Button(window,text="View all",width=12,command=view_command)
 b1.grid(row=2,column=3)
 
-b1=Button(window,text="Search",width=12)
-b1.grid(row=3,column=3)
+b2=Button(window,text="Search",width=12,command=search_command)
+b2.grid(row=3,column=3)
 
-b1=Button(window,text="Add",width=12)
-b1.grid(row=4,column=3)
+b3=Button(window,text="Add",width=12,command=add_command)
+b3.grid(row=4,column=3)
 
-b1=Button(window,text="Update",width=12)
-b1.grid(row=5,column=3)
+b4=Button(window,text="Update",width=12,command=update_command)
+b4.grid(row=5,column=3)
 
-b1=Button(window,text="Delete",width=12)
-b1.grid(row=6,column=3)
+b5=Button(window,text="Delete",width=12,command=delete_command)
+b5.grid(row=6,column=3)
 
-b1=Button(window,text="Close",width=12)
-b1.grid(row=7,column=3)
+b6=Button(window,text="Close",width=12,command=window.destroy)
+b6.grid(row=7,column=3)
 
 
 window.mainloop()
