1532b834f8e18d1cc00b48e98d3501716ac60c3e
Rajat Rawat <rajatrawat88@gmail.com>
2019-09-24 09:00:57 -0500

    update create.sh

--- python/studyMaterial/udacity/Lesson2/Project2/create.sh
+++ python/studyMaterial/udacity/Lesson2/Project2/create.sh
@@ -4,3 +4,6 @@ aws cloudformation create-stack \
 --template-body file://$2 \
 --parameters file://$3 \
 --region=us-west-2 \
+
+aws cloudformation create-stack --stack-name project2Udacity --template-body file://project2Udacity.yml --parameters file://project2Udacity.json --region=us-west-2
+aws cloudformation update-stack --stack-name project2Udacity --template-body file://project2Udacity.yml --parameters file://project2Udacity.json --region=us-west-2
